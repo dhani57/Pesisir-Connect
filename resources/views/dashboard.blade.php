1eba169a76771f94932486bddc450499f87c633d
Ramadhani <dhnahmad32@gmail.com>
2026-06-10 21:38:53 +0700

    fix bug

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,11 +1,5 @@
 <x-app-layout>
-    <x-slot name="header">
-        <div class="flex items-center justify-between">
-            <h2 class="font-bold text-2xl text-gray-900 leading-tight">
-                {{ __('Dashboard') }}
-            </h2>
-        </div>
-    </x-slot>
+
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-8">
